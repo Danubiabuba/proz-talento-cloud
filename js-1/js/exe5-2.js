ff0e6ad43008ad828761430f4c039466a4ee2cd2
Danubiabuba <danubiaalves184@gmail.com>
2024-05-27 14:57:46 -0300

    exercicio de desenvolvimento 5.2

---
+++ js-1/js/exe5-2.js
@@ -0,0 +1,11 @@
+function operacoesBasicas(num1, num2) {
+    console.log(`
+    ${num1} + ${num2} = ${num1 + num2}
+    ${num1} - ${num2} = ${num1 - num2}
+    ${num1} * ${num2} = ${num1 * num2}
+    ${num1} / ${num2} = ${num1 / num2.toFixed(1)}
+    `);
+}
+
+  // Exemplo de uso
+operacoesBasicas(4, 5);
