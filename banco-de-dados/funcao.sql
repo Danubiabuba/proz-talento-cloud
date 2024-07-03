c9b14d42b47353a2f0a70ab6aae949d5e1259f3f
Danubiabuba <danubiaalves184@gmail.com>
2024-07-03 13:11:23 -0300

    utilizando função sql

---
+++ banco-de-dados/funcao.sql
@@ -0,0 +1,3 @@
+-- Chamando a função para obter o total de clientes cadastrados em 2024-07-03 (exemplo)
+SELECT total_clientes_cadastrados_por_dia('2024-07-03');
+
---
+++ banco-de-dados/loja.sql
@@ -0,0 +1,2 @@
+use loja;
+INSERT INTO clientes (nome, data_cadastro) VALUES ('caio castro', '2014-08-05');
